387f8bdabbdadeb7b2bd2460162442e70ee6b086
Jiri Knesl <jiri@flexiana.com>
2024-05-21 14:25:24 +0100

    Change expiration algo for JWT tokens to expire in 1000000 hours instead of 1)

--- gen-jwt.rb
+++ gen-jwt.rb
@@ -5,7 +5,7 @@ payload = {
   role: ARGV[1], # Role (user or admin)
   iss: 'xiana-api',
   aud: 'api-consumer',
-  exp: (Time.now + 3600).to_i  # Token expiration time (1 hour from now)
+  exp: (Time.now + 3600000000).to_i  # Token expiration time (1M hour from now)
 }
 
 secret = 'your-secret-key'  # Replace with your secret key
